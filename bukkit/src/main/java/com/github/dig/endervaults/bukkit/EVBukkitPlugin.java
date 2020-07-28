9f440b259f119ac705ebf2e617631598e2a9dd34
Joseph <joseph42@live.co.uk>
2020-07-28 18:29:41 +0100

    Rename nms module

--- bukkit/src/main/java/com/github/dig/endervaults/bukkit/EVBukkitPlugin.java
+++ bukkit/src/main/java/com/github/dig/endervaults/bukkit/EVBukkitPlugin.java
@@ -148,6 +148,16 @@ public class EVBukkitPlugin extends JavaPlugin implements EnderVaultsPlugin {
     private void setNMSProvider() throws InvalidMinecraftVersionException {
         MinecraftVersion version = getVersion();
         switch (version) {
+            case v1_8_R3:
+            case v1_9_R1:
+            case v1_9_R2:
+            case v1_10_R1:
+            case v1_11_R1:
+            case v1_12_R1:
+            case v1_13_R1:
+            case v1_13_R2:
+            case v1_14_R1:
+            case v1_15_R1:
             case v1_16_R1:
                 NMSProvider.set(new v1_16_R1NMS(), version);
                 break;
