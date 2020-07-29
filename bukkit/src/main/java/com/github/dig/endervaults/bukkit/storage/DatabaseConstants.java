25de0e1bad72f5a42be0554bb2839f63254febe2
Joseph <joseph42@live.co.uk>
2020-07-29 18:46:37 +0100

    Change to longtext from text

--- bukkit/src/main/java/com/github/dig/endervaults/bukkit/storage/DatabaseConstants.java
+++ bukkit/src/main/java/com/github/dig/endervaults/bukkit/storage/DatabaseConstants.java
@@ -2,7 +2,7 @@ package com.github.dig.endervaults.bukkit.storage;
 
 public class DatabaseConstants {
 
-    public static final String SQL_CREATE_TABLE_VAULT = "CREATE TABLE IF NOT EXISTS `%s` ( `id` VARCHAR(36) NOT NULL , `owner_uuid` VARCHAR(36) NOT NULL , `size` INT(8) NOT NULL , `contents` TEXT NOT NULL , PRIMARY KEY (`id`, `owner_uuid`) )";
+    public static final String SQL_CREATE_TABLE_VAULT = "CREATE TABLE IF NOT EXISTS `%s` ( `id` VARCHAR(36) NOT NULL , `owner_uuid` VARCHAR(36) NOT NULL , `size` INT(8) NOT NULL , `contents` LONGTEXT NOT NULL , PRIMARY KEY (`id`, `owner_uuid`) )";
     public static final String SQL_CREATE_TABLE_VAULT_METADATA = "CREATE TABLE IF NOT EXISTS `%s` ( `id` VARCHAR(36) NOT NULL , `owner_uuid` VARCHAR(36) NOT NULL , `name` VARCHAR(16) NOT NULL , `value` TEXT NOT NULL , PRIMARY KEY (`id`, `owner_uuid`, `name`) )";
 
     public static final String SQL_SELECT_VAULT_BY_ID_AND_OWNER = "SELECT * FROM `%s` WHERE `id` = ? AND `owner_uuid` = ?";
